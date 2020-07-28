8a86dc66f551eff1fb91ac5b7ed59bfa56bfbe12
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-07-28 16:19:23 -0400

    skills-done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,19 +10,66 @@ import post from '../post.svg';
 
 export default function About() {
   return (
+    <>
+      <h1 className='tools'>My Tool Kit:</h1>
     <div className='about-container'>
-      <img id='sql' classname='.sql' src={sql} alt='img' />
-      <i className="fab fa-html5"></i>
-      <img id='css' src={css} alt='img'/>
-      <i className="fab fa-js js"></i>
-      <i class="fab fa-react"></i>
-      <i class="fab fa-node"></i>
-      <img id='express' src={express} alt='img'/>
-      <img id='ruby' src={ruby} alt='img'/>
-      <img id='rails' src={rails} alt='img'/>
-      <img id='mongo' src={mongodb} alt='img'/>
-      <img id='postgresql' className='post' src={post} alt='img'/>
+      
+      <div className='box-sql p'>
+        <img id='sql' classname='.sql' src={sql} alt='img' />
+        <p className='description'>SQL</p>
+      </div>
+
+      <div className='box-html p'>
+          <i className="fab fa-html5"></i>
+          <p className='description'>HTML</p>
+      </div>
+      
+      <div className='box-css p'>
+          <img id='css' src={css} alt='img' />
+          <p className='description'>CSS</p>
+      </div>
+      
+      <div className='box-js p'>
+          <i className="fab fa-js js"></i>
+          <p className='description'>JAVASCRIPT</p>
+      </div>
+      
+      <div className='box-react p'>
+          <i class="fab fa-react"></i>
+          <p className='description'>JS-REACT</p>
+      </div>
+      
+      <div className='box-node p'>
+          <i class="fab fa-node"></i>
+          <p className='description'>NODE.JS</p>
+      </div>
+      
+      <div className='box-express p'>
+          <img id='express' src={express} alt='img' />
+          <p className='description'>EXPRESS</p>
+      </div>
+
+      <div className='box-ruby p'>
+          <img id='ruby' src={ruby} alt='img' />
+          <p className='description'>RUBY</p>
+      </div>
+      
+      <div className='box-rails p'>
+          <img id='rails' src={rails} alt='img' />
+          <p className='description'>RUBY ON RAILS</p>
+      </div>
+      
+      <div className='box-mongo p' >
+          <img id='mongo' src={mongodb} alt='img' />
+          <p className='description'>MONGODB</p>
+      </div>
+      
+      <div className='box-post p'>
+          <img id='postgresql' className='post' src={post} alt='img' />
+          <p className='description'>POSTGRESQL</p>
+      </div>
 
     </div>
+      </>
   )
 }
