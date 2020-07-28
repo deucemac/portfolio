5daf8f055e67fdedae0eed7a95818d61b4f8c8cf
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-07-28 11:29:37 -0400

    home-page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <>
       <div className='home-container'>
-        
+        <h1 className='greeting'>Hi I'm Gerand, here's what I've been up to!</h1>
+        <Link><h1 className='see-more'>V</h1></Link>
       </div>
     </>
   )
