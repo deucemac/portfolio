f9682d928fde4540c17a2ba1ce06638906c81db0
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-07-28 13:58:13 -0400

    logosadded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Applist from '../src/components/Applist'
 import { Route } from 'react-router-dom'
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import sql from '../sql.svg';
 import './Skills.css';
 import css from '../css.svg';
+import express from '../express.svg';
+import ruby from '../ruby.svg';
+import rails from '../rails.svg';
+import mongodb from '../mongodb.svg'
+import post from '../post.svg';
 
 export default function About() {
   return (
     <div className='about-container'>
-      <img id='sql' classname='.sql' src={sql} />
-      <i class="fab fa-html5"></i>
-      <img id='css' src={css} />
-    
-
+      <img id='sql' classname='.sql' src={sql} alt='img' />
+      <i className="fab fa-html5"></i>
+      <img id='css' src={css} alt='img'/>
+      <i className="fab fa-js js"></i>
+      <i class="fab fa-react"></i>
+      <i class="fab fa-node"></i>
+      <img id='express' src={express} alt='img'/>
+      <img id='ruby' src={ruby} alt='img'/>
+      <img id='rails' src={rails} alt='img'/>
+      <img id='mongo' src={mongodb} alt='img'/>
+      <img id='postgresql' className='post' src={post} alt='img'/>
 
     </div>
   )
